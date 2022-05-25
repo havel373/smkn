8989ab49d2b747261005d1d0bd01580237ae5bcb
Ricky Ananda Pardomuan Sitorus <75471225+rickyananada@users.noreply.github.com>
2022-05-25 18:47:51 +0700

    Update Login
    Update Login

--- resources/views/themes/app/head.blade.php
+++ resources/views/themes/app/head.blade.php
@@ -19,6 +19,7 @@
     <!--end::Page Vendor Stylesheets-->
     <!--begin::Global Stylesheets Bundle(used by all pages)-->
     <link href="{{asset('css/confirm.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('css/login.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
