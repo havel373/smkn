f21c980ddab75e374ddec011cebdea9bd14a72aa
Ricky Ananda Pardomuan Sitorus <75471225+rickyananada@users.noreply.github.com>
2022-05-25 18:58:23 +0700

    color
    color

--- resources/views/themes/app/header.blade.php
+++ resources/views/themes/app/header.blade.php
@@ -61,7 +61,13 @@
                     <div class="cursor-pointer symbol symbol-30px symbol-md-40px"
                         data-kt-menu-trigger="click" data-kt-menu-attach="parent"
                         data-kt-menu-placement="bottom-end">
-                        <img src="{{asset('img/avatars/admin.png')}}" alt="user" />
+                        @if(Auth::user()->role=='o')
+                            <img src="{{asset('img/avatars/admin.png')}}" alt="user" style="background-color: black;" />
+                        @elseif(Auth::user()->role=='g')
+                            <img src="{{asset('img/avatars/admin.png')}}" alt="user" style="background-color: rgb(97, 97, 186);" />
+                        @else
+                            <img src="{{asset('img/avatars/admin.png')}}" alt="user" style="background-color: red;" />
+                        @endif
                     </div>
                     <!--begin::User account menu-->
                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px"
@@ -70,9 +76,19 @@
                         <div class="menu-item px-3">
                             <div class="menu-content d-flex align-items-center px-3">
                                 <!--begin::Avatar-->
-                                <div class="symbol symbol-50px me-5">
-                                    <img alt="Logo" src="{{asset('img/avatars/admin.png')}}" />
-                                </div>
+                                @if(Auth::user()->role=='o')
+                                    <div class="symbol symbol-50px me-5" style="background-color: black;">
+                                        <img alt="Logo" src="{{asset('img/avatars/admin.png')}}" />
+                                    </div>
+                                @elseif(Auth::user()->role=='g')
+                                    <div class="symbol symbol-50px me-5" style="background-color: rgb(97, 97, 186);">
+                                        <img alt="Logo" src="{{asset('img/avatars/admin.png')}}" />
+                                    </div>
+                                @else
+                                    <div class="symbol symbol-50px me-5" style="background-color: red;">
+                                        <img alt="Logo" src="{{asset('img/avatars/admin.png')}}" />
+                                    </div>
+                                @endif
                                 <!--end::Avatar-->
                                 <!--begin::Username-->
                                 <div class="d-flex flex-column">
